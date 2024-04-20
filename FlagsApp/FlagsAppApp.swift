799c7bf0848e97f74be3b8d2bc6d6bdc54ee0ea0
Vlad Klunduk <klundukvlad40@gmail.com>
2024-04-20 15:51:08 +0200

    Initial Commit

---
+++ FlagsApp/FlagsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsAppApp.swift
+//  FlagsApp
+//
+//  Created by Vlad Klunduk on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlagsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
